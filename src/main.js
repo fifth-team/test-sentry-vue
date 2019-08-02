a3d13c2f5f7735581667a8154601b18ea183016a
lyc <lycxiaowu@gmail.com>
2019-08-02 18:04:26 +0800

    add sentry

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
+Sentry.init({
+  dsn: 'https://7ab75385e1ae4286a7052a7338988f67@sentry.io/1519993',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+})
 Vue.config.productionTip = false
 
 new Vue({
